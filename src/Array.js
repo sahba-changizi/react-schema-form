bd1077a0dc06b343b015a5c05a513a6cd5305ca9
SahbaCh <sahba.ch@gmail.com>
2019-12-25 16:36:27 +0330

    fix bug in removing empty value when the model value is 0 and field has number type

--- src/Array.js
+++ src/Array.js
@@ -246,7 +246,7 @@ class ArrayComponent extends Component<Props, State> {
         }
         return (
             <div className={classes.root}>
-                <div style={{ display: "flex", ...form.style }}>
+                <div style={{ display: "flex" }}>
                     <FormLabel
                         required={form.required}
                         className={classes.title}
